756cceb663b3b2b21a7f20c4caf57e67d919b230
Pranav Singh <pranav.singh@intel.com>
2025-04-21 11:24:26 +0530

    init
    Signed-off-by: Pranav Singh <pranav.singh@intel.com>

---
+++ script.py
@@ -0,0 +1,73 @@
+import csv
+import openvino_genai as ov_genai
+from sklearn.metrics import accuracy_score
+
+model_path = "TinyLlama"
+pipe = ov_genai.LLMPipeline(model_path, "NPU")
+
+input = ["biology.csv", "english.csv", "maths.csv", "physics.csv", "chemistry.csv"]
+
+class Inference:
+    prompt: str
+    output: str
+    expected_output: str
+    accuracy: float
+
+    def __init__(self, prompt: str, output: str):
+        self.prompt = prompt
+        self.output = output
+        self.expected_output = ""
+        self.accuracy = 0.0
+
+    def inference(self) -> str:
+        self.expected_output = pipe.generate(self.prompt, max_new_tokens=100, do_sample=False)
+
+    def evaluate(self):
+        output = {
+            "prompt": self.prompt,
+            "output": self.output,
+        }
+        expected_output = {
+            "prompt": self.prompt,
+            "expected_output": self.expected_output,
+        }
+        correct_responses = sum(
+            1 for original, new in zip(expected_output, output)
+            if original["output"] == new["expected_output"]
+        )
+        self.accuracy = correct_responses / len(output) 
+
+def write(filename: str, inference: Inference):
+    outputfile = "out" + "/" + filename 
+    with open(outputfile, mode='w') as csv_file:
+        fieldnames = ['prompt', 'output', 'expected_output', 'accuracy']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({
+            'prompt': inference.prompt,
+            'output': inference.output,
+            'expected_output': inference.expected_output,
+            'accuracy': inference.accuracy
+        })
+
+def read(filename: str):
+    with open(file=filename, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            inf = Inference(row['prompt'], row['output'])
+            inf.inference()
+            inf.evaluate()
+            write(filename=filename, inference=inf)
+            line_count += 1
+        print(f'Processed {line_count} lines.')
+
+if __name__ == "__main__":
+    for i in input:
+        read(i)
+        print(f'Processed {i} file.')
+    print("All files processed successfully.")
+    print("Inference and evaluation completed.")
+
