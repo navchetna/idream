e6b5fb9315d30dd466f9580fd7ee22adb1eb46a5
TanmayUdupa <tanmaypraveenudupa@gmail.com>
2025-05-02 04:50:10 +0000

    Change prompt

--- model.py
+++ model.py
@@ -10,38 +10,32 @@ from config import EMBED_MODEL
 tei_embedding_endpoint = os.getenv("TEI_ENDPOINT")
 
 def load_knowledgeBase():
-        if tei_embedding_endpoint:
-            # create embeddings using TEI endpoint service
-            embedder = HuggingFaceEndpointEmbeddings(model=tei_embedding_endpoint)
-        else:
-            # create embeddings using local embedding model
-            embedder = HuggingFaceBgeEmbeddings(model_name=EMBED_MODEL)
-        DB_FAISS_PATH = 'vectorstore/db_faiss'
-        db = FAISS.load_local(DB_FAISS_PATH, embedder, allow_dangerous_deserialization=True)
-        return db
+    if tei_embedding_endpoint:
+        embedder = HuggingFaceEndpointEmbeddings(model=tei_embedding_endpoint)
+    else:
+        embedder = HuggingFaceBgeEmbeddings(model_name=EMBED_MODEL)
+    DB_FAISS_PATH = 'vectorstore/db_faiss'
+    db = FAISS.load_local(DB_FAISS_PATH, embedder, allow_dangerous_deserialization=True)
+    return db
 
 knowledgeBase = load_knowledgeBase()
 
-prompt_template_without_context = PromptTemplate.from_template(
-    """You are a helpful assistant
+prompt_template_with_context = """
+### Context: {context}
 
-Question:
-{question}
+### Question: {question}
 
-Answer:"""
-)
+### Answer:
+You are a helpful, respectful, and honest assistant. Answer clearly in 2–3 sentences (under 100 tokens). Use only relevant information from the context. If you don't know, say you don't know.
+"""
 
-prompt_template_with_context = PromptTemplate.from_template(
-    """You are a helpful assistant. Use only the below context and answer the question. Remove any unecessary information
+prompt_template_without_context = """
+### Question: {question}
 
-Context:
-{context}
+### Answer:
+You are a helpful, respectful, and honest assistant. Answer clearly in 2–3 sentences (under 100 tokens). If you don't know, say you don't know.
+"""
 
-Question:
-{question}
-
-Answer:"""
-)
 
 pipe_llama = ov_genai.LLMPipeline("Llama-3.2-3B-Instruct", "CPU")
 pipe_qwen = ov_genai.LLMPipeline("Qwen2.5-3B-Instruct", "CPU")
@@ -62,17 +56,17 @@ def generate_text(class_range: str, query: str):
     result_qwen = pipe_qwen.generate(prompt_without_context, max_new_tokens=100, do_sample=False)
     result_phi = pipe_phi.generate(prompt_without_context, max_new_tokens=100, do_sample=False)
 
-    similar_docs = knowledgeBase.similarity_search(query, k=5)
+    similar_docs = knowledgeBase.similarity_search(query, k=3)
     filtered_docs = [
         doc for doc in similar_docs if int(doc.metadata['grade']) in grade_list
-    ] 
+    ]
     context = format_docs(filtered_docs)
     prompt_with_context = prompt_template_with_context.format(context=context, question=query)
 
     result_llama_rag = pipe_llama.generate(prompt_with_context, max_new_tokens=100, do_sample=False)
     result_qwen_rag = pipe_qwen.generate(prompt_with_context, max_new_tokens=100, do_sample=False)
     result_phi_rag = pipe_phi.generate(prompt_with_context, max_new_tokens=100, do_sample=False)
-    
+
     return {
         "answer_llama": result_llama,
         "answer_qwen": result_qwen,
